b173116433b99766053a8fd047aa302a46e23bb1
Paul Mederos <paulmederos@me.com>
2013-04-14 14:17:14 -0400

    Refactored views into partials so template can be used on other pages. Added small logic for sidebar active pages to work.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Brim::Application.routes.draw do
   root :to => 'pages#home'
   
   match "/portfolio" => "pages#portfolio", :as => :portfolio
+  match "/library" => "pages#library", :as => :library
 end
