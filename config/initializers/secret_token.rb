1a11b4b15387fde22a1cbb32847bf94b6520117e
Paul Mederos <paulmederos@me.com>
2013-04-14 10:04:07 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brim::Application.config.secret_token = 'b024bef4c09ce81f1b27d0bf94e5421704dd12227833d17ed8f2d31a101a128c606b05a518f0e4bbb5dad5d273dc16f10bd18601185a26a8a3a0c0095cb7f424'
