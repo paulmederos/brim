ee5f98b50381e80880d444503eda2921d9f91499
Paul Mederos <paulmederos@me.com>
2013-04-14 21:39:44 -0400

    Wrapped up skills sets on Portfolio page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Brim::Application.routes.draw do
   
   match "/portfolio" => "pages#portfolio", :as => :portfolio
   match "/library" => "pages#library", :as => :library
+
+  match "/portfolio/cfpb-internal-tools" => "pages#cfpb-internal-tools", :as => :cfpb_internal_tools
 end
