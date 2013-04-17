b09756d0bfdee4dddb7a9fae4b6f505ca468d30a
Paul Mederos <paulmederos@me.com>
2013-04-17 09:00:54 -0400

    Library page completed.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Brim::Application.routes.draw do
   match "/library" => "pages#library", :as => :library
 
   match "/portfolio/cfpb-internal-tools" => "pages#cfpb-internal-tools", :as => :cfpb_internal_tools
+  match "/portfolio/cfpb-consumer-complaints" => "pages#cfpb-consumer-complaints", :as => :cfpb_Consumer_complaints
 end
