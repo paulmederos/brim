2187622124e7f72237bbc2e87b458e508d3f796d
Paul Mederos <paulmederos@me.com>
2013-04-23 22:22:40 -0400

    Strategic plan page added, along with screenshots.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Brim::Application.routes.draw do
   match "/library" => "pages#library", :as => :library
 
   match "/portfolio/cfpb-internal-tools" => "pages#cfpb-internal-tools", :as => :cfpb_internal_tools
+  match "/portfolio/cfpb-strategic-plan" => "pages#cfpb-strategic-plan", :as => :cfpb_strategic_plan
   match "/portfolio/cfpb-consumer-complaints" => "pages#cfpb-consumer-complaints", :as => :cfpb_consumer_complaints
   match "/portfolio/cfpb-paying-for-college" => "pages#cfpb-paying-for-college", :as => :cfpb_paying_for_college
 end
