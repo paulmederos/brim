358ad5d248e37e7abc6a965051d3fc0bf2cfc59b
Paul Mederos <paulmederos@me.com>
2013-04-23 22:03:27 -0400

    Optimized images, added custom headers to pages.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Brim::Application.routes.draw do
   match "/library" => "pages#library", :as => :library
 
   match "/portfolio/cfpb-internal-tools" => "pages#cfpb-internal-tools", :as => :cfpb_internal_tools
-  match "/portfolio/cfpb-consumer-complaints" => "pages#cfpb-consumer-complaints", :as => :cfpb_Consumer_complaints
+  match "/portfolio/cfpb-consumer-complaints" => "pages#cfpb-consumer-complaints", :as => :cfpb_consumer_complaints
+  match "/portfolio/cfpb-paying-for-college" => "pages#cfpb-paying-for-college", :as => :cfpb_paying_for_college
 end
